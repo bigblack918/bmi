bc48d37573c0c6b39855b35960a65aa957d13450
i_leotsai1 <leotsai1@fareastone.com.tw>
2019-07-16 14:33:22 +0800

    restructure code

--- app/src/main/java/com/example/leo/bmi/MainActivity.java
+++ app/src/main/java/com/example/leo/bmi/MainActivity.java
@@ -15,36 +15,49 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initViews();
+        setListensers();
+    }
 
-        Button button = (Button) findViewById(R.id.button);
-        button.setOnClickListener(calBmi);
+    private Button button_calc;
+    private EditText num_height;
+    private EditText num_weight;
+    private TextView show_result;
+    private TextView show_suggest;
+
+    private void initViews(){
+        button_calc = (Button)findViewById(R.id.button);
+        num_height = (EditText)findViewById(R.id.height);
+        num_weight = (EditText)findViewById(R.id.weight);
+        show_result = (TextView)findViewById(R.id.result);
+        show_suggest = (TextView)findViewById(R.id.suggest);
+    }
+
+    private void setListensers(){
+        button_calc.setOnClickListener(calBmi);
     }
 
     private View.OnClickListener calBmi = new View.OnClickListener() {
         @Override
         public void onClick(View view) {
             DecimalFormat nf = new DecimalFormat("0.00");
-            EditText fieldheight = (EditText)findViewById(R.id.height);
-            EditText fieldweight = (EditText)findViewById(R.id.weight);
             //身高
-            double height = Double.parseDouble(fieldheight.getText().toString())/100;
+            double height = Double.parseDouble(num_height.getText().toString())/100;
             //體重
-            double weight = Double.parseDouble(fieldweight.getText().toString());
+            double weight = Double.parseDouble(num_weight.getText().toString());
             //計算出BMI值
             double BMI = weight / (height*height);
 
             //結果
-            TextView result = (TextView)findViewById(R.id.result);
-            result.setText(getText(R.string.bmi_result) + nf.format(BMI));
+            show_result.setText(getText(R.string.bmi_result) + nf.format(BMI));
 
             //建議
-            TextView fieldsuggest = (TextView)findViewById(R.id.suggest);
             if(BMI > 25) //太重了
-                fieldsuggest.setText(R.string.advice_heavy);
+                show_suggest.setText(R.string.advice_heavy);
             else if(BMI < 20) //太輕了
-                fieldsuggest.setText(R.string.advice_light);
+                show_suggest.setText(R.string.advice_light);
             else //剛剛好
-                fieldsuggest.setText(R.string.advice_average);
+                show_suggest.setText(R.string.advice_average);
         }
     };
 }
